44c8201cd4f4b21c4bcc53201b328a56be718c0d
rhasrie82 <33564425+rhasrie82@users.noreply.github.com>
2018-05-09 00:46:58 -0400

    I've added the files

---
+++ config/connection..js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+
+
+ //Local Database Configuration with Mongoose
+
+ mongoose.connect("mongodb://localhost/allthenewsdb", function(error)
+
+ 	{if(error) throw error;
+
+ 	console.log("Database connected");
+
+ });
+
+
+
